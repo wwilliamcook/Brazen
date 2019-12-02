132bd419567f83c5219d1a4b9c526cd22ec9416d
Weston Cook <wwilliamcook@gmail.com>
2019-12-02 03:52:27 -0800

    Create build_test.cpp

---
+++ test/build_test.cpp
@@ -0,0 +1,17 @@
+/*
+ * build_test.cpp
+ * 
+ * Tests whether a program can compile after including the files in include/.
+ */
+
+#include "../include/tuple.h"
+#include "../include/particle.h"
+#include "../include/simulator.h"
+
+using namespace Brazen;
+
+int main() {
+    Simulator<3> simulator;
+
+    return 0;
+}
