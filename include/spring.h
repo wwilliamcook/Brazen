9e255c0a158db5b2e5de0ca5dc7a2a55ded7571c
Weston Cook <wwilliamcook@gmail.com>
2019-12-07 03:31:51 -0800

    Update Spring

--- include/spring.h
+++ include/spring.h
@@ -20,40 +20,42 @@
 #include "tuple.h"
 #include "particle.h"
 
+#define EPSILON 0.000001
+
 
 namespace Brazen {
 	/*
 	 * Enum class: SpringForceType
 	 * ---------------------------
-	 * Specifies restoring force types in a spring-like particle connection.
+	 * Specifies restoring force types in particle connections.
 	 */
 	enum class SpringForceType {
-		SPRING,    // Behaves like an ideal spring
-		CONSTANT,  // Always applies the same amount of force to retain shape
-		STRONG,    // Unable to be compressed or pulled when force_coefficient is 1
-		NO_FORCE,  // Does not exert any force
-		MUSCLE     // Force can be controlled dynamically
+		RIGID,       // Unable to be compressed or pulled when force_coefficient is 1
+		SPRING,      // Behaves like an ideal spring
+		INV_SQUARE,  // Applies force according to the inverse square law
+		NONE         // Applies no force
 	};
 
 	/*
 	 * Enum class: SpringType
 	 * ----------------------
-	 * Provides preset Spring connection types.
+	 * Provides preset Spring force types.
 	 */
 	enum class SpringType {
-		BUNGEE,  // Compressed: no force | Stretched: spring force
-		BOUNCY_ROPE,  // Compressed: no force | Stretched: constant force
-		ROPE,  // Compressed: no force | Stretched: unstretchable
-		SPRING,  // Compressed: spring force | Stretched: spring force
-		SPRING2,  // Compressed: spring force | Stretched: constant force
-		GELATIN,  // Compressed: spring force | Stretched: unstretchable
-		STRETCHY_BOUNCY,  // Compressed: constant force | Stretched: spring force
-		BOUNCY,  // Compressed: constant force | Stretched: unstretchable
-		BOUNCY2,  // Compressed: constant force | Stretched: constant force
-		SLINKY,  // Compressed: incompressible | Stretched: spring force
-		SLINKY2,  // Compressed: incompressible | Stretched: constant force
-		STRONG,  // Compressed: incompressible | Stretched: unstretchable
-		MUSCLE  // Exert a specific force regardless of deformation direction
+		RIGID,   // Compressed: RIGID  | Stretched: RIGID
+		SPRING,  // Compressed: SPRING | Stretched: SPRING
+
+		BOUNCY,           // Compressed: INV_SQUARE | Stretched: INV_SQUARE
+		BOUNCY2,          // Compressed: INV_SQUARE | Stretched: RIGID
+
+		GELATIN,  // Compressed: SPRING | Stretched: RIGID
+
+		SLINKY,   // Compressed: RIGID | Stretched: SPRING
+		SLINKY2,  // Compressed: RIGID | Stretched: INV_SQUARE
+
+		ROPE,         // Compressed: NONE | Stretched: RIGID
+		BOUNCY_ROPE,  // Compressed: NONE | Stretched: SPRING
+		BUNGEE        // Compressed: NONE | Stretched: INV_SQUARE
 	};
 
 	/*
@@ -64,113 +66,141 @@ namespace Brazen {
 	template <std::uint8_t _Size>
 	class Spring {
 		/**************************** ATTRIBUTES *****************************/
-		std::uint32_t p1_index, p2_index;
+	private:
+		Particle<_Size> *p1, *p2;
 
-		double natural_length;  // Distance at which the connected particles can be at equilibrium
-		double compression_force_strength,  // Strength of force to apply when the connection is compressed
-			tension_force_strength;  // Strength of force to apply when the connection is stretched
+		double natural_length,  // Distance at which force is zero
+		       comp_strength,   // Strength of compression-restoring force
+			   tens_strength;   // Strength of tension-restoring force
 
-		SpringForceType compression_force_type,  // Type of force to apply when the connection is compressed
-			tension_force_type;  // Type of force to apply when the connection is stretched
-		double deformation_coefficient;  // Mutability of "natural_length" due to prolonged deformation. Value of 0 allows no deformation; value of 1 provides no resistance to deformation.
-
+		SpringForceType comp_type,  // Type of compression-restoring force
+			            tens_type;  // Type of tension-restoring force
+		double deform_coef;  // Maliability of natural_length due to prolonged
+			                 // deformation. Value of 0 allows no deformation.
+							 // Value of 1 provides no resistance to deformation.
+	public:
 		/********************* CONSTRUCTORS/DESTRUCTORS **********************/
 
-		Spring(std::uint32_t p1_index, std::uint32_t p2_index, double natural_length, double force_strength) :  // Behaves like an ideal spring
-			p1_index(p1_index), p2_index(p2_index),
+		/*
+		 * Basic constructor
+		 * -----------------
+		 * Initializes the Spring to behave like an ideal spring.
+		 */
+		Spring(double natural_length, double force_strength) :
+			p1(nullptr), p2(nullptr),
 			natural_length(natural_length),
-			compression_force_strength(force_strength),
-			tension_force_strength(force_strength),
-			compression_force_type(SpringForceType::SPRING),
-			tension_force_type(SpringForceType::SPRING),
-			deformation_coefficient(0)
-		{
-			if (p1 == p2) {  // Attempting to attach both ends of connection to the same particle
-				std::cerr << "Error: cannot attach a particle to itself." << std::endl;
-				exit(EXIT_FAILURE);
-			}
-		}
+			comp_strength(force_strength),
+			tens_strength(force_strength),
+			comp_type(SpringForceType::SPRING),
+			tens_type(SpringForceType::SPRING),
+			deform_coef(0)
+		{}
 
-		Spring(std::uint32_t p1_index, std::uint32_t p2_index, double natural_length,
-			double compression_force_strength, double tension_force_strength,
-			SpringType spring_type, double deformation_coefficient) :  // Behave like an ideal spring
-			p1_index(p1_index), p2_index(p2_index),
+		/*
+		 * Advanced constructor
+		 * --------------------
+		 * Initializes the Spring to behave in the specified way.
+		 */
+		Spring(double natural_length,
+			   double comp_strength, double tens_strength,
+			   SpringType spring_type, double deform_coef) :
+			p1(nullptr), p2(nullptr),
 			natural_length(natural_length),
-			deformation_coefficient(deformation_coefficient)
+			comp_strength(comp_strength),
+			tens_strength(tens_strength),
+			deform_coef(deform_coef)
 		{
-			if (p1 == p2) {  // Attempting to attach both ends of connection to the same particle
-				std::cerr << "Error: cannot attach a particle to itself." << std::endl;
-				exit(EXIT_FAILURE);
-			}
-
 			// Select a pair of SpringForceTypes to match the desired connection behavior
 			switch (spring_type) {
-			case SpringType::BUNGEE:
-				compression_force_type = SpringForceType::NO_FORCE;
-				tension_force_type = SpringForceType::SPRING;
-				break;
-			case SpringType::BOUNCY_ROPE:
-				compression_force_type = SpringForceType::NO_FORCE;
-				tension_force_type = SpringForceType::CONSTANT;
-				break;
-			case SpringType::ROPE:
-				compression_force_type = SpringForceType::NO_FORCE;
-				tension_force_type = SpringForceType::STRONG;
+			case SpringType::RIGID:
+				comp_type = SpringForceType::RIGID;
+				tens_type = SpringForceType::RIGID;
 				break;
 			case SpringType::SPRING:
-				compression_force_type = SpringForceType::SPRING;
-				tension_force_type = SpringForceType::SPRING;
-				break;
-			case SpringType::SPRING2:
-				compression_force_type = SpringForceType::SPRING;
-				tension_force_type = SpringForceType::CONSTANT;
-				break;
-			case SpringType::GELATIN:
-				compression_force_type = SpringForceType::SPRING;
-				tension_force_type = SpringForceType::STRONG;
-				break;
-			case SpringType::STRETCHY_BOUNCY:
-				compression_force_type = SpringForceType::CONSTANT;
-				tension_force_type = SpringForceType::SPRING;
+				comp_type = SpringForceType::SPRING;
+				tens_type = SpringForceType::SPRING;
 				break;
 			case SpringType::BOUNCY:
-				compression_force_type = SpringForceType::CONSTANT;
-				tension_force_type = SpringForceType::STRONG;
+				comp_type = SpringForceType::INV_SQUARE;
+				tens_type = SpringForceType::INV_SQUARE;
 				break;
 			case SpringType::BOUNCY2:
-				compression_force_type = SpringForceType::CONSTANT;
-				tension_force_type = SpringForceType::CONSTANT;
+				comp_type = SpringForceType::INV_SQUARE;
+				tens_type = SpringForceType::RIGID;
+				break;
+			case SpringType::GELATIN:
+				comp_type = SpringForceType::SPRING;
+				tens_type = SpringForceType::RIGID;
 				break;
 			case SpringType::SLINKY:
-				compression_force_type = SpringForceType::STRONG;
-				tension_force_type = SpringForceType::SPRING;
+				comp_type = SpringForceType::RIGID;
+				tens_type = SpringForceType::SPRING;
 				break;
 			case SpringType::SLINKY2:
-				compression_force_type = SpringForceType::STRONG;
-				tension_force_type = SpringForceType::CONSTANT;
+				comp_type = SpringForceType::RIGID;
+				tens_type = SpringForceType::INV_SQUARE;
 				break;
-			case SpringType::STRONG:
-				compression_force_type = SpringForceType::STRONG;
-				tension_force_type = SpringForceType::STRONG;
+			case SpringType::ROPE:
+				comp_type = SpringForceType::NONE;
+				tens_type = SpringForceType::RIGID;
 				break;
-			case SpringType::MUSCLE:
-				compression_force_type = SpringForceType::MUSCLE;
-				tension_force_type = SpringForceType::MUSCLE;
-				throw std::exception("SpringType \"MUSCLE\" has not been implemented yet.");
+			case SpringType::BOUNCY_ROPE:
+				comp_type = SpringForceType::NONE;
+				tens_type = SpringForceType::SPRING;
+				break;
+			case SpringType::BUNGEE:
+				comp_type = SpringForceType::NONE;
+				tens_type = SpringForceType::INV_SQUARE;
+				break;
+			default:
+				std::cerr << "Error: unimplemented SpringType.\n";
+				exit(EXIT_FAILURE);
+				break;
 			}
 		}
 
+		/*
+		 * Copy constructor
+		 */
 		Spring(const Spring<_Size>& s) :
-			p1_index(s.p1_index), p2_index(s.p2_index),
+			p1(s.p1), p2(s.p2),
 			natural_length(s.natural_length),
-			compression_force_strength(s.compression_force_strength),
-			tension_force_strength(s.tension_force_strength),
-			compression_force_type(s.compression_force_type),
-			tension_force_type(s.tension_force_type),
-			deformation_coefficient(s.deformation_coefficient)
+			comp_strength(s.comp_strength),
+			tens_strength(s.tens_strength),
+			comp_type(s.comp_type),
+			tens_type(s.tens_type),
+			deform_coef(s.deform_coef)
 		{}
 
 		/********************** MANIPULATION FUNCTIONS ***********************/
+
+		/*
+		 * Function: setEndpoints
+		 * ----------------------
+		 * Sets the endpoint Particles of this Spring.
+		 * 
+		 * Preconditions:
+		 *   either p1 == p2 == nullptr
+		 *       or p1 != p2, p1 != nullptr, p2 != nullptr
+		 */
+		void setEndpoints(Particle<_Size>* p1, Particle<_Size>* p2) {
+			// Assert preconditions
+			if (p1 == nullptr || p2 == nullptr) {
+				if (p1 != p2) {
+					std::cerr << "Spring Error: calling setEndpoints(...) with one NULL Particle*\n";
+					exit(EXIT_FAILURE);
+				}
+			}
+			else {
+				if (p1 == p2) {
+					std::cerr << "Spring Error: calling setEndpoints(...) when p1 == p2.\n";
+					exit(EXIT_FAILURE);
+				}
+			}
+			// Continue with function
+			this->p1 = p1;
+			this->p2 = p2;
+		}
 
 		/*
 		 * Function: update
@@ -178,62 +208,51 @@ namespace Brazen {
 		 * Applies the designated forces to the two particles according to the
 		 * Spring type.
 		 */
-		void update(std::vector<Particle<_Size> >& particles) {
-			// Acquire pointers to the particles that need to be updated
-			Particle<_Size> *p1 = particles.at(p1_index),
-				*p2 = particles.at(p2_index);
-
+		void update(void) {
 			Tuple<_Size> P1toP2 = p2->pos - p1->pos;
 			double distance = magnitude(P1toP2);
 			P1toP2 = unit(P1toP2);
-			double displacement = distance - natural_length;  // Extent two which the spring is compressed
+			double displacement = distance - natural_length;  // Distance the spring is compressed
+
 			double force_magnitude;  // Magnitude of attractive force between the two particles
-			Tuple<_Size> force_vector;
+			Tuple<_Size> force_vector(false);
 
-			SpringForceType force_type_to_check;
-			double force_coefficient_to_use;
+			SpringForceType force_type;
+			double force_coef;
 
-			double invinvMassSum;  // Inverse sum of inverse masses of the two particles (used for STRONG connection types)
-			Tuple<_Size> m_delta_pos(false), m_delta_vel(false), delta_F(false);
-			double total_change, mutual_change_vector;
+			double invinvMassSum;  // Inverse sum of inverse masses of the two particles (used for RIGID connection types)
+			Tuple<_Size> m_delta_pos(false),
+			             m_delta_vel(false),
+						 delta_F(false),
+						 mutual_change_vector(false);
+			double total_change;
 
-			if (distance != naturalLength) {  // Only apply force if the connection is deformed
+			if (distance != natural_length) {  // Only apply force if the connection is deformed
 				// Deform the natural length according to the deformation coefficient
-				natural_length += displacement * deformation_coefficient;
+				natural_length += displacement * deform_coef;
 				displacement = distance - natural_length;
 
 				// Determine how to apply the force
-				if (distance < natural_length) {
-					force_coefficient_to_use = compression_force_strength;
-					force_type_to_check = compression_force_type;
+				if (distance < natural_length) {  // Compressed
+					force_coef = comp_strength;
+					force_type = comp_type;
 				}
-				else {
-					force_coefficient_to_use = tension_force_strength;
-					force_type_to_check = tension_force_type;
+				else {  // Stretched
+					force_coef = tens_strength;
+					force_type = tens_type;
 				}
 
 				// Determine the magnitude of the attractive force
-				switch (force_type_to_check) {
-				case SpringForceType::NO_FORCE:  // Do not apply any force
-					return;
-				case SpringForceType::SPRING:  // Follow Hooke's law
-					force_magnitude = force_coefficient_to_use * displacement;
-					break;
-				case SpringForceType::CONSTANT:  // Apply a constant force
-					if (displacement >= 0)  // Too far apart => positive attractive force
-						force_magnitude = force_coefficient_to_use;
-					else  // Too close together => negative attractive force
-						force_magnitude = -force_coefficient_to_use;
-					break;
-				case SpringForceType::STRONG:  // Do not allow any deformation
+				switch (force_type) {
+				case SpringForceType::RIGID:  // Disallow deformation
 					// Calculate the inverse sum of inverse masses
 					invinvMassSum = p1->invMass + p2->invMass;
 					invinvMassSum = (invinvMassSum > 0) ? (1. / invinvMassSum) : 0;  // Zero means that both particles have infinite mass
 
 					// Calculate mass*position change -- Total displacement should cause distance to equal naturalLength
-					m_delta_pos = P1toP2 * (force_coefficient_to_use * displacement * invinvMassSum);
+					m_delta_pos = P1toP2 * (force_coef * displacement * invinvMassSum);
 					// Calculate mass*velocity change -- Total velocity change should zero the relative velocity along the colinear axis
-					m_delta_vel = P1toP2 * (force_coefficient_to_use * (dot(p1->vel, P1toP2) - dot(p2->vel, P1toP2)) * invinvMassSum);
+					m_delta_vel = P1toP2 * (force_coef * (dot(p1->vel, P1toP2) - dot(p2->vel, P1toP2)) * invinvMassSum);
 
 					if (invinvMassSum > 0) {  // At least one of the particles has finite mass
 						// Apply position change
@@ -245,7 +264,7 @@ namespace Brazen {
 						p2->m_delta_vel += m_delta_vel;
 
 						// Calculate the mass**acceleration change -- Total acceleration change should zero the relative acceleration along the colinear axis
-						delta_F = P1toP2 * (force_coefficient_to_use * (p1->invMass*dot(p1->F, P1toP2) - p2->invMass*dot(p2->F, P1toP2)) * invinvMassSum);
+						delta_F = P1toP2 * (force_coef * (p1->invMass*dot(p1->F, P1toP2) - p2->invMass*dot(p2->F, P1toP2)) * invinvMassSum);
 
 						// Apply acceleration change
 						p1->F -= delta_F;
@@ -265,18 +284,23 @@ namespace Brazen {
 
 					// Adjust force
 					// Total force change should zero the relative force along the colinear axis
-					totalChange = dot(p1->F, P1toP2) - dot(p2->F, P1toP2);
-					mutualChangeVector = P1toP2 * (force_coefficient_to_use * total_change / 2);  // Change in force from the midpoint per particle
-					p1->F -= mutualChangeVector;
-					p2->F += mutualChangeVector;
+					total_change = dot(p1->F, P1toP2) - dot(p2->F, P1toP2);
+					mutual_change_vector = P1toP2 * (force_coef * total_change / 2.);  // Change in force from the midpoint per particle
+					p1->F -= mutual_change_vector;
+					p2->F += mutual_change_vector;
 
 					return;  // No additional forces need to be applied
-				case SpringForceType::MUSCLE:
-					return;  // NOT IMPLEMENTED
+				case SpringForceType::SPRING:  // Hooke's law
+					force_magnitude = force_coef * displacement;
 					break;
+				case SpringForceType::INV_SQUARE:  // Inverse square law
+					force_magnitude = force_coef / (distance * distance + EPSILON);
+					break;
+				case SpringForceType::NONE:  // No force
+					return;
 				}
 
-				forceVector = P1toP2 * force_magnitude;
+				force_vector = P1toP2 * force_magnitude;
 				p1->F += force_vector;
 				p2->F -= force_vector;
 			}
