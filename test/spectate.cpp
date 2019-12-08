d00949a58fd6ff853d72deac8b3a6e9f8732fe4b
Weston Cook <wwilliamcook@gmail.com>
2019-12-07 21:48:54 -0800

    Update test/spectate.cpp - runs

--- test/spectate.cpp
+++ test/spectate.cpp
@@ -19,7 +19,7 @@
 #include "../include/simulator.h"
 #include "../include/localSpectator.h"
 
-#define DIMENSIONS 2
+#define DIMENSIONS 3
 #define NUM_CYCLES 10
 #define CYCLE_PERIOD .1
 
@@ -36,31 +36,22 @@ int main() {
     Simulator simulator;
     VideoOutput video_output(simulator, "Spectate Test",
                              640, 480,
-                             Camera(.64, .48, Vect({ 0., 0. }), Vect({ 1., 0. })));
-    int i, j, k;
+                             Camera(.064, .048, Vect({ -5, 0, 0 }), Vect({ 1, 0, 0 })),
+                             0.05);
 
     // Add some Particles
-    simulator.addParticle(Particle(Vect({ 1, 0 }), 1));
-    simulator.addParticle(Particle(Vect({ 0, 1 }), 2));
-    simulator.addParticle(Particle(Vect({ -1, 0 }), 3));
-    simulator.addParticle(Particle(Vect({ 0, -1 }), Vect({ .05, 0. }), 4));
+    simulator.addParticle(Particle(Vect({ 1, 0, 1 }), 1));
+    simulator.addParticle(Particle(Vect({ 0, 1, 0 }), 2));
+    simulator.addParticle(Particle(Vect({ -1, 0, -1 }), 3));
+    simulator.addParticle(Particle(Vect({ 0, -1, 0 }), Vect({ .05, 0., 0. }), 4));
 
     // Add some Springs
-    simulator.addSpring(0, 1, Spring(.5, 5.));
+    simulator.addSpring(0, 1, Spring(.5, .2));
 
-    for (i = 0; i < NUM_CYCLES; i++) {
-        simulator.updateState(CYCLE_PERIOD);
-        simulator.updateOutput();
-
-        // Print the state
-        for (j = 0; j < simulator.getOutput().size(); j++) {
-            for (k = 0; k < DIMENSIONS; k++)
-                cout << simulator.getOutput().at(j).pos[k] << " ";
-            
-            cout << "\t";
-        }
-        cout << endl;
-    }
+    simulator.start();
+    video_output.start();
+    video_output.join();
+    simulator.stop();
 
     return 0;
 }
